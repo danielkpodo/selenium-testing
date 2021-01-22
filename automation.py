cbbd453e0865e921a18e409f05f2a0a483ed135e
narh <kpododanielnarh@gmail.com>
2021-01-22 11:43:28 +0000

    grab dummy content from webpage

--- automation.py
+++ automation.py
@@ -6,7 +6,9 @@ browser.maximize_window()  # you can resize with media queries
 
 browser.get(test_url)
 
-assert "Selenium Easy Demo" in browser.title
+# assert "Selenium Easy Demo" in browser.title test if a word exists in title
 
-
-# 
+# Utilizing Selectors
+button = browser.find_element_by_class_name('btn-default')
+btn_text = button.get_attribute('innerHTML')
+print(btn_text);
