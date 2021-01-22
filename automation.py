26e511cfac1b83a4ad5fe1f71d77109d51411c8e
narh <kpododanielnarh@gmail.com>
2021-01-22 12:43:02 +0000

    simulated user actions

--- automation.py
+++ automation.py
@@ -8,7 +8,21 @@ browser.get(test_url)
 
 # assert "Selenium Easy Demo" in browser.title test if a word exists in title
 
-# Utilizing Selectors
-button = browser.find_element_by_class_name('btn-default')
-btn_text = button.get_attribute('innerHTML')
-print(btn_text);
+# Utilizing Selectors - refer to [http://allselenium.info/python-selenium-commands-cheat-sheet-frequently-used/]
+show_message_button = browser.find_element_by_class_name('btn-default')
+# btn_text = button.get_attribute('innerHTML')
+# print(btn_text)
+
+assert "Show Message" in browser.page_source
+
+user_message = browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('Daniel Narh is a prolific programmer! ')
+show_message_button.click()
+
+
+# Closing the browser
+# browser.close -> closes the current browser
+# browser.quit -> quits the entire driver
+
+browser.close()
--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
 # Automation Testing Using Selenium
 
 - To interact with the page element what we have to do is use selectors
--
+- Using waits will allow you to simulate better actions like humans
