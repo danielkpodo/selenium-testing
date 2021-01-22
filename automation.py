3ab1c267c299664af0f2d3e36edadc8f1fc7cbfb
narh <kpododanielnarh@gmail.com>
2021-01-22 11:16:06 +0000

    open chrome browser via terminal

--- automation.py
+++ automation.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome('./chromedriver')
-print(browser)
+test_url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
+browser.maximize_window()  # you can resize with media queries
+
+browser.get(test_url)
+
+assert "Selenium Easy Demo" in browser.title
+
+
+# 
---
+++ notes.md
@@ -0,0 +1,4 @@
+# Automation Testing Using Selenium
+
+- To interact with the page element what we have to do is use selectors
+-
